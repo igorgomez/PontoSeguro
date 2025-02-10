a497a449b2d66a64bdcdf99931e91a61cac0e8d9
igorgomez <igorgomez@gmail.com>
2025-02-10 02:08:45 -0300

    ajustes no build do ghp

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ponto-eletronico/', // Adiciona o base path para o GitHub Pages
+  base: '/PontoSeguro/', // Adiciona o base path para o GitHub Pages
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
