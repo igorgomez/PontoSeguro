571a92fa14bd57dff2e4f80e8530475d7646c1b0
Igor Gomes <igorgomez@users.noreply.github.com>
2025-02-13 06:08:13 +0000

    inclusão da versão automatizada

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,13 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react()
+    replace({
+      preventAssignment: true,
+      'process.env.VERSION': JSON.stringify(process.env.npm_package_version),
+    }),
+  ],
   base: '/PontoSeguro/', // Adiciona o base path para o GitHub Pages
   optimizeDeps: {
     exclude: ['lucide-react'],
