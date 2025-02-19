8e3167a0866951a47fe0a68831ee0d89f00d26df
igorgomez <igorgomez@gmail.com>
2025-02-19 15:49:33 -0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import replace from '@rollup/plugin-replace'; // Adicione esta linha
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    replace({
-      preventAssignment: true,
-      values: { // Estrutura correta para o plugin replace
-        'process.env.VERSION': JSON.stringify(
-          process.env.npm_package_version || '0.0.0'
-        ),
-      }
-    }),
-  ],
-  base: '/PontoSeguro/',
+  plugins: [react()],
+  base: '/PontoSeguro/', // Adiciona o base path para o GitHub Pages
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
